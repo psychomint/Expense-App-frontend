5d201a66567106a4f78e2c87d5ef3d824239ae5b
Brijesh Shukla <bkshukla229@gmail.com>
2025-09-06 12:27:38 +0530

    Frontend Prject Setup
    Signed-off-by: Brijesh Shukla <bkshukla229@gmail.com>

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+export default function App () {
+    return(
+         <div className="">
+            <div className="h-50 w-1/4 border-2 border-red-600">
+
+            </div>
+            <div className="h-60 w-1/4 border-2 border-green-600">
+
+            </div>
+        </div>
+    )
+}
